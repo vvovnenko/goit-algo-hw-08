e2d09a4da213ad9bc9d7b491b9e37575b4592804
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-01-26 20:28:28 +0200

    fix result cost

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import heapq
 
 
 def calc_min_cost(cables: list):
+    cost = 0
     h = cables[:]
     heapq.heapify(h)
 
@@ -10,12 +11,13 @@ def calc_min_cost(cables: list):
     while len(h) > 1:
         cable_1 = heapq.heappop(h)
         cable_2 = heapq.heappop(h)
-        print(f'connecting cables {cable_1} and {cable_2}')
-        heapq.heappush(h, cable_1 + cable_2)
+        connection_cost = cable_1 + cable_2
+        print(f'connecting cables {cable_1} and {cable_2} with cost {connection_cost}')
+        cost += connection_cost
+        heapq.heappush(h, connection_cost)
         print('heap = ', h)
 
-
-    return h.pop()
+    return cost
 
 
 print(calc_min_cost([1, 2, 4, 10]))
